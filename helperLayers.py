5440193a7c897112a02c11b3ad31d9266ff7194c
sushil-khyalia <skhyalia2014@gmail.com>
2019-03-08 02:54:17 +0530

    Creating .py files

---
+++ helperLayers.py
@@ -0,0 +1,45 @@
+import torch
+import torchsummary
+import numpy as np
+from torch import nn
+
+class ResBlock(nn.Module):
+    def __init__(self,kernel_size,channels,padding,stride):
+        super(ResBlock,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=channels,out_channels=channels,kernel_size=kernel_size,padding=padding,stride=stride)
+        self.batch_norm1 = nn.BatchNorm2d(num_features=channels)
+        self.prelu1 = nn.PReLU()
+        self.conv2 = nn.Conv2d(in_channels=channels,out_channels=channels,kernel_size=kernel_size,padding=padding,stride=stride)
+        self.batch_norm2 = nn.BatchNorm2d(num_features=channels)
+    def forward(self,inp):
+        x = self.conv1(inp)
+        x = self.batch_norm1(x)
+        x = self.prelu1(x)
+        x = self.conv2(x)
+        x = self.batch_norm2(x)
+        output = x + inp
+        return output
+
+class SubPixelConv(nn.Module):
+    def __init__(self,kernel_size,in_channels,out_channels,padding,stride):
+        super(SubPixelConv,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=kernel_size,padding=padding,stride=stride)
+        self.pixel_shuffler1 = nn.PixelShuffle(2)
+        self.prelu1 = nn.PReLU()
+    def forward(self,inp):
+        x = self.conv1(inp)
+        x = self.pixel_shuffler1(x)
+        output = self.prelu1(x)
+        return output
+
+class ConvBlock(nn.Module):
+    def __init__(self,kernel_size,in_channels,out_channels,padding,stride):
+        super(ConvBlock,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=kernel_size,padding=padding,stride=stride)
+        self.batch_norm1 = nn.BatchNorm2d(num_features=out_channels)
+        self.leaky_relu1 = nn.LeakyReLU(negative_slope=0.2)
+    def forward(self,inp):
+        x = self.conv1(inp)
+        x = self.batch_norm1(x)
+        output = self.leaky_relu1(x)
+        return output
---
+++ loss.py
@@ -0,0 +1,38 @@
+import torch
+import torchsummary
+import numpy as np
+from torch import nn
+from torchvision.models import vgg19
+
+class PerceptualLoss(nn.Module):
+    def __init__(self):
+        super(PerceptualLoss,self).__init__()
+        self.mse_loss = nn.MSELoss()
+        self.vgg = list(vgg19(pretrained=True).children())[0]
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+    def forward(self,generated_image,ground_truth):
+        y = ground_truth
+        y_hat = generated_image
+        loss = 0
+        for name,module in self.vgg._modules.items():
+            if isinstance(module,nn.MaxPool2d):
+                loss = loss + self.mse_loss(y_hat/12.75,y.detach()/12.75)
+            y = module(y)
+            y_hat = module(y_hat)
+        return loss
+
+class AdversarialLoss(nn.Module):
+    def __init__(self):
+        super(AdversarialLoss,self).__init__()
+    def forward(self,probabilities):
+        return -torch.log(probabilities).sum()
+
+class DiscriminatorLoss(nn.Module):
+    def __init__(self):
+        super(DiscriminatorLoss,self).__init__()
+    def forward(self,probabilities,is_ground_truth):
+        if is_ground_truth:
+            return -torch.log(probabilities).sum()
+        else:
+            return torch.log(probabilities).sum()
